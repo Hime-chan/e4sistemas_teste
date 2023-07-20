4634bb0cf8ad155f7a7ad89e484b65245fe47eca
Claudia Cavalcante Fonseca <himehimur@gmail.com>
2023-07-20 18:19:14 -0300

    Update usuarios_apagar.php

--- usuarios_apagar.php
+++ usuarios_apagar.php
@@ -1,5 +1,5 @@
 <?  include_once('init.php'); 
-	if (!$_SESSION['u_status']) {echo "Você não está logado."; header('Location: https://purrfect.codes/e4sistemas_teste/');}
+	if (!$_SESSION['u_status']) {echo "Você não está logado."; header('Location: '.$link_atual);}
 $_GET['tipo']='usuario'; 
 $databaseObj->query("update ".$_GET['tipo']." set excluido=1 where Id=".$_GET['Id']);
 include_once('listar.php');
