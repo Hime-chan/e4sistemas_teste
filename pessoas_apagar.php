f34d11550e9bd39921bceb7af86f2aa98b5b423c
Claudia Cavalcante Fonseca <himehimur@gmail.com>
2023-07-20 18:18:27 -0300

    Update pessoas_apagar.php

--- pessoas_apagar.php
+++ pessoas_apagar.php
@@ -1,5 +1,5 @@
 <?  include_once('init.php'); 
-	if (!$_SESSION['u_status']) {echo "Você não está logado."; header('Location: https://purrfect.codes/e4sistemas_teste/');}
+	if (!$_SESSION['u_status']) {echo "Você não está logado."; header('Location: '.$link_atual);}
 $_GET['tipo']='pessoa'; 
 $databaseObj->query("delete from telefone where pessoa_id=".$_GET['Id']);
 $databaseObj->query("delete from ".$_GET['tipo']." where Id=".$_GET['Id']);
