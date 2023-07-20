09993801b67af08d435ce47562f63760005ebbe0
Claudia Cavalcante Fonseca <himehimur@gmail.com>
2023-07-20 18:14:53 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <html>
 <head>
-	<title>Teste de Conhecimento - E4sistemas - Claudia C. F.</title>
+	<title>Teste de Conhecimento - Orientação a objeto em Php - Claudia C. F.</title>
 	<link rel="icon" href="favico_teste_ccf.png" type="image/png">
 <? include('init.php'); ?>
 </head>
 <body>
-<h1>Teste de Conhecimento - E4sistemas - Claudia C. F.</h1>
+<h1>Teste de Conhecimento - Orientação a objeto em Php - Claudia C. F.</h1>
 <div id='container'>
 <div id='main'>
 <? include($_SESSION['u_usuario']?($_GET['pg']?$_GET['pg'].'.php':'menu.php'):'login.php');?>
