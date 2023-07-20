4669004a54560bbabed8a46b17e673bd0df3e088
Claudia Cavalcante Fonseca <himehimur@gmail.com>
2023-07-20 18:00:38 -0300

    Update init.php

--- init.php
+++ init.php
@@ -5,6 +5,7 @@ session_start();
 include("classes.php");
 include('exceptions.php');
 include("functions.php");
-$databaseObj = new Database($server, $usuario, $senha, $database); // Preencher aqui!
+$databaseObj = new Database("mysql.purrfect.codes", "purrfect", "E4sistemas", "purrfect");
 $databaseObj->connect();
+$link_atual = "https://purrfect.codes/php_teste/";
 ?>
